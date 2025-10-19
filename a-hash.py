7645d80943c4189bf3e44de2954ce27223b8d4b6
aooxd <164191158+aooxd@users.noreply.github.com>
2025-10-20 00:41:05 +0300

    Create a-hash.py

---
+++ a-hash.py
@@ -0,0 +1,8 @@
+phone_directory = {
+    "Lena": "+380671010101",
+    "Andriy": "+380932020202",
+    "Julia": "+380993030303",
+}
+
+def find_phone_in_directory(name):
+    return phone_directory.get(name)
